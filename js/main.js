a789c57d9d06552a7a81d1f06af2a741fa5ae071
Victor <victorallard59@gmail.com>
2022-03-11 12:22:45 +0100

    remplacement de la fonction 'afficher' dans le JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-function afficher() {
-    const input = document.getElementById("password");
-    if (input.type === "password");
-    {
-        input.type = "text";
+function afficher(){
+    let x = document.getElementById("myInput");
+    if(x.type === "password"){
+        x.type = "text";
     } else {
-        input.type = "password";
+        x.type = "password"
     }
 }
+
+
+// étape 1 : Je créé une variable x, et lui associe l'élément input du mot de passe, pour améliorer la lisibilité de la fonction ensuite. 
+// etape 2: Je crée une première condition: si le type de mon input est égal à password, alors je lui attribue le type text pour que le mot de passe soit visible.
+// Sinon, le type de l'input est password
+// Dans le html, j'appelle cette fonction avec un élément onclick au niveau de mon élément checkbox.

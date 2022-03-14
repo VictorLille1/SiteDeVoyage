b9c725adcd1113f84285940ead0ae5209901c3cc
Victor <victorallard59@gmail.com>
2022-03-14 18:24:36 +0100

    creation fonction pour changer bg color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-function afficher(){
+function afficher() {
     let x = document.getElementById("myInput");
-    if(x.type === "password"){
+    if (x.type === "password") {
         x.type = "text";
     } else {
         x.type = "password"
@@ -12,3 +12,18 @@ function afficher(){
 // etape 2: Je crée une première condition: si le type de mon input est égal à password, alors je lui attribue le type text pour que le mot de passe soit visible.
 // Sinon, le type de l'input est password
 // Dans le html, j'appelle cette fonction avec un élément onclick au niveau de mon élément checkbox.
+
+
+// colors tableau
+let colors = ['red', 'yellow', 'green', 'blue', 'pink'];
+
+// bouton get
+let button = document.getElementById('button');
+
+button.addEventListener('click', function () {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+
+    let colorBody = document.getElementById('colorBody');
+
+    colorBody.style.background = randomColor;
+})
